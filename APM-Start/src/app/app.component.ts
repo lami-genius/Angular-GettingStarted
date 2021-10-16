73cbe3a0f1c152b4066fee857d7073c5c57be467
F-B-Lami <fonyuyborislami@gmail.com>
2021-10-16 19:40:18 +0200

    MVP complete

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',  // pm - product managment hence root, root app component
-  template: `<div>
-  <h1>{{pageTitle}}</h1>
-  <p>My first component</p>
-  </div>`
+  templateUrl: './app.component.html'   // using directives pm-products from product-list component
 })
 
 
